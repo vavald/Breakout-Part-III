f50705f6abb50bd6e643c9f6bf0cbed7712c99d0
Larry Van Wallendael <100205404+larrywallendael@users.noreply.github.com>
2022-05-24 21:14:56 +0200

    Testing for echarge

--- Breakout Part III/src/breakout/utils/LinksTest.java
+++ Breakout Part III/src/breakout/utils/LinksTest.java
@@ -1,4 +1,4 @@
-package breakout.utils;
+package other;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import breakout.BreakoutFacade;
 import breakout.radioactivity.*;
+import breakout.utils.Point;
+import breakout.utils.Vector;
 
 class LinksTest {
 	//Parameters to define balls & alphas
@@ -26,17 +28,26 @@ class LinksTest {
 		Alpha a4 = fac.createAlpha(defPoint, 700, downSpeed);
 		
 		//Link testing
+		assertEquals(b1.getEcharge(), 1);		//##
 		b1.linkTo(a1);
+		assertEquals(b1.getEcharge(), -1);		//## one connected alpha > uneven > negatif && alphas have max 1 connection...
 		b1.linkTo(a2);
+		assertEquals(b1.getEcharge(), 1);		//##
 		assertEquals(b1.getLinkedAlphas().size(), 2);
 		assertTrue(a1.getLinkedBalls().contains(b1));
 		assertTrue(a2.getLinkedBalls().contains(b1));
 		
 		//unLink testing
 		b1.unLink(a2);
+		assertEquals(b1.getEcharge(), -1);		//##
 		assertEquals(b1.getLinkedAlphas().size(), 1);
 		assertFalse(a2.getLinkedBalls().contains(b1));
 		
+		System.out.print("step 2");
+		b2.linkTo(a3);
+		b2.linkTo(a4);
+		b3.linkTo(a4);
+		assertEquals(b2.getEcharge(), 2);
 		
 		
 	}
